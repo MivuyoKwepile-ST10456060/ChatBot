07ba5bf9a23586c3dc8b99b4387bbe41e947daa6
Mivuyo Kwepile <ST10456060@rcconnect.edu.za>
2026-04-10 14:46:50 +0200

    Removed unnecessary things

--- ChatBot/AsciiArt.cs
+++ ChatBot/AsciiArt.cs
@@ -12,13 +12,6 @@
   ╚██████╗   ██║   ██████╔╝███████╗██║  ██║
    ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝
 
-        >>> CYBER SECURITY TERMINAL v1.0 <<<
-        >>> Initializing Secure Environment...
-
-         Firewall Active
-         Encryption Enabled
-         Threat Detection Online
-
         Welcome to the Cybersecurity Awareness Bot 
 ";
         }
